1326ddd528329d3e8513a4751f929e91616d13de
warthundervlk <warthundervlk@gmail.com>
2021-09-13 03:00:13 -0300

    Se ocultan los elementos
    Se ocultan los elementos que anteriormente eran visible y se veian sus colores

--- main.js
+++ main.js
@@ -1,48 +1,38 @@
 let objetos = {
     cuadrado1 : {
-        foundMatch : false
     },
     cuadrado2 : {
-        foundMatch : false
     },
     cuadrado3 : {
-        foundMatch : false
     },
     cuadrado4 : {
-        foundMatch : false
     },
     cuadrado5 : {
-        foundMatch : false
     },
-    cuadrado6 : { 
-        foundMatch : false
+    cuadrado6 : {
     },
     cuadrado7 : {
-        foundMatch : false
     },
     cuadrado8 : { 
-        foundMatch : false
     },
     cuadrado9 : {   
-        foundMatch : false
     },
-    cuadrado10 : {   
-        foundMatch : false
+    cuadrado10 : {    
     },
     cuadrado11 : {      
-        foundMatch : false
     },
-    cuadrado12 : {
-        foundMatch : false
+    cuadrado12 : {  
     }
 }
 
 
 let seleccionCuadro1;
 let seleccionCuadro2;
+let contador = 0
+let contadorPares = 0
 asignarColores()
 habilitarUsuario() 
-
+let resultado = document.querySelector(".resultado")
 
 
 function asignarColores() {
@@ -70,35 +60,100 @@ function habilitarUsuario() {
     })
 }
 
+document.querySelector(".reiniciar").onclick = reiniciar
+
 function compararCuadro(e) {
     
     if(seleccionCuadro1 === undefined) {
         seleccionCuadro1 = e.target
         agregarBordesCuadrado(seleccionCuadro1)
-    } else {
+        mostrarCuadro(seleccionCuadro1)
+    } else if(seleccionCuadro1 != e.target) {
         seleccionCuadro2 = e.target
         agregarBordesCuadrado(seleccionCuadro2)
+        mostrarCuadro(seleccionCuadro2)
+    } else {
+        eliminarBordesCuadrado(seleccionCuadro1)
+        ocultarCuadro(seleccionCuadro1)
+        seleccionCuadro1 = undefined
     }
-
+    
     if(seleccionCuadro1 && seleccionCuadro2) {
-        if(objetos[seleccionCuadro1.id].color === objetos[seleccionCuadro2.id].color) {
-            seleccionCuadro1.style.visibility = "hidden"
-            seleccionCuadro2.style.visibility = "hidden"
-            eliminarBordesCuadrado(seleccionCuadro1)
-            eliminarBordesCuadrado(seleccionCuadro2)
-            seleccionCuadro1 = undefined
-            seleccionCuadro2 = undefined
+        setTimeout(function () {
+            if(objetos[seleccionCuadro1.id].color === objetos[seleccionCuadro2.id].color) {
 
-        } else {
-            eliminarBordesCuadrado(seleccionCuadro1)
-            eliminarBordesCuadrado(seleccionCuadro2)
-            seleccionCuadro1 = undefined
-            seleccionCuadro2 = undefined
+                seleccionCuadro1.style.visibility = "hidden"
+                seleccionCuadro2.style.visibility = "hidden"
+                
+                let elementoPadreSeleccionCuadro1 = seleccionCuadro1.parentElement
+                let elementoPadreSeleccionCuadro2 = seleccionCuadro2.parentElement
+                elementoPadreSeleccionCuadro1.style.backgroundColor = objetos[seleccionCuadro1.id].color
+                elementoPadreSeleccionCuadro2.style.backgroundColor = objetos[seleccionCuadro2.id].color
 
-        }
-    }
+
+
+                ocultarCuadro(seleccionCuadro1)
+                ocultarCuadro(seleccionCuadro2)
+                eliminarBordesCuadrado(seleccionCuadro1)
+                eliminarBordesCuadrado(seleccionCuadro2)
+                
+                seleccionCuadro1 = undefined
+                seleccionCuadro2 = undefined
+                
+                contadorPares++
+                contador++
+
+                if(contadorPares === 6){
+                    document.querySelector(".container").style.visibility = "hidden"
+                    
+                    resultado.style.visibility = "visible"
+                    resultado.textContent = `Finalizaste el juego en ${contador} turnos!!!`
+                    document.querySelector(".reiniciar").style.visibility = "visible"
+
+                }
+            } else {
+                ocultarCuadro(seleccionCuadro1)
+                ocultarCuadro(seleccionCuadro2)
+                eliminarBordesCuadrado(seleccionCuadro1)
+                eliminarBordesCuadrado(seleccionCuadro2)
+
+                seleccionCuadro1 = undefined
+                seleccionCuadro2 = undefined
+                
+                contador++
+            } 
+        },750)}
+        
+        
 }
 
+function reiniciar() {
+    ocultarResultado()
+    reiniciarCuadros()
+
+}
+
+function reiniciarCuadros() {
+    document.querySelectorAll(".cuadro").forEach(function($cuadro) {
+        reinciarCuadro($cuadro)
+    })
+    
+    asignarColores()
+    contador = 0
+    contadorPares = 0
+}
+
+function reinciarCuadro(cuadro) {
+    cuadro.style.visibility = "visible"
+    let elementoPadreCuadro = cuadro.parentElement
+    elementoPadreCuadro.style.backgroundColor = ""
+}
+
+function ocultarResultado() {
+    document.querySelector(".container").style.visibility = "visible"
+    resultado.style.visibility = "hidden"
+    document.querySelector(".reiniciar").style.visibility = "hidden"
+}
 
 function agregarBordesCuadrado(cuadro) {
     cuadro.style.border = "1px solid"
@@ -109,3 +164,18 @@ function eliminarBordesCuadrado(cuadro) {
     cuadro.style.border = ""
     cuadro.style[`border-color`] = ""
 }
+
+function mostrarCuadro(cuadro) {
+    cuadro.style.opacity = 1
+}
+
+function ocultarCuadro(cuadro) {
+    cuadro.style.opacity = 0
+}
+
+/* 
+
+
+
+Agregar una pagina de inicio para seleccionar la dificultad.
+*/
